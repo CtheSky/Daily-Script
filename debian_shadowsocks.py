909744159d6b9f65ae640bda8b02b4f0bf961a98
Tianhong Chu <cthesky@yeah.net>
2018-09-23 22:28:56 +0800

    :sparkles: setup shadowsocks server remotely

--- debian_shadowsocks.py
+++ debian_shadowsocks.py
@@ -37,7 +37,9 @@ def establish_connected_client(host):
 
 
 def exec_command_and_print_stdout(client, command):
-    print('Executing command: {}'.format(command).center(50, '='))
+    print('Start executing command'.center(50, '='))
+    print(command)
+    print('Command Output'.center(50, '='))
 
     _, stdout, stderr = client.exec_command(command)
 
@@ -46,19 +48,62 @@ def exec_command_and_print_stdout(client, command):
         print(str(output, "utf8"))
 
 
-def main():
-    client = establish_connected_client('gcp')
-
-    # install necessary packages
+def install_packages(client):
+    """install necessary packages"""
     commands = [
-        'sudo apt-get update',
-        'sudo apt-get install git',
-        'sudo apt-get install python-pip',
+        'sudo apt-get -y update',
+        'sudo apt-get -y install git',
+        'sudo apt-get -y install python-pip',
         'sudo pip install git+https://github.com/shadowsocks/shadowsocks.git@master'
     ]
 
     for cmd in commands:
         exec_command_and_print_stdout(client, cmd)
+
+
+def config_and_setup_server(client):
+    cmd_tpl = 'sudo ssserver -s {server_address} -p {server_port} -k {password} -m {method} -d start'
+
+    class Option:
+        def __init__(self, name, default):
+            self.name = name
+            self.default = default
+            self.value = None
+
+    options = [
+        Option(name='server_address', default='127.0.0.1'),
+        Option(name='server_port', default='2333'),
+        Option(name='password', default='123456'),
+        Option(name='method', default='aes-256-cfb')
+    ]
+
+    while True:
+        print('Please specify the config option for the server:')
+        for option in options:
+            option.value = input('Option name : [{}], Default value: [{}]\n'.format(option.name, option.default))
+
+        kwargs = {option.name: option.default if not option.value else option.value for option in options}
+        cmd = cmd_tpl.format(**kwargs)
+
+        y_or_n = input('Is [{}] correct? [Y/n]'.format(cmd))
+        if not y_or_n or y_or_n.lower() in ['y', 'yes']:
+            client.exec_command(cmd)
+            sys.exit(0)
+
+
+def main():
+    import argparse
+
+    parser = argparse.ArgumentParser('Read ssh config, connect to the remote, '
+                                     'install packages and setup shadowsocks server. (Only work for debian now)')
+    parser.add_argument('--host', help='host configured in .ssh/config file')
+
+    args = parser.parse_args()
+    host = args.host
+
+    client = establish_connected_client(host)
+    install_packages(client)
+    config_and_setup_server(client)
 
 
 if __name__ == '__main__':
